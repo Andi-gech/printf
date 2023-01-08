716b44b72722124593a4080cedf44a4f6edf008b
Andi-gech <andi.fab23@gmail.com>
2023-01-08 16:01:24 -0500

    first

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef PRINT_H
+#define PRINT_H
+#ifndef EOF
+#define EOF '\0'
+#endif
+#ifndef NULL
+#define NULL (void *)0
+#endif
+int _strlen(char *s);
+int numlen(long int n);
+char *intstr(long int n);
+int _putchar(int);
+int putnum(char *);
+int _puts(char *);
+int _printf(const char *format, ...);
+#endif
